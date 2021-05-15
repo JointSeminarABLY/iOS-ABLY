59ae6a9dab2792b4e4d839a12dec1e5d3310aba9
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-16 00:41:59 +0900

    [ADD] Detail CVC 추가

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/CVC/DetailItemCVC.swift
@@ -0,0 +1,18 @@
+//
+//  DetailItemCVC.swift
+//  ABLY-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/16.
+//
+
+import UIKit
+
+class DetailItemCVC: UICollectionViewCell {
+    static let identifier = "DetailItemCVC"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
