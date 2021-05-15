014f47b56040a94198ece3111ffa735256aca79b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-15 23:08:34 +0900

    [ADD] HotRank Cell 추가

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/MainHotRankTVC.swift
@@ -0,0 +1,24 @@
+//
+//  MainHotRankTVC.swift
+//  ABLY-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/15.
+//
+
+import UIKit
+
+class MainHotRankTVC: UITableViewCell {
+    static let identifier = "MainHotRankTVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
