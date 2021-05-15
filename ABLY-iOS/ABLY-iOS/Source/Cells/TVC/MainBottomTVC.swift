32902fe4d130dcade1ecc3e803c504c8eee6ae5d
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-15 22:00:19 +0900

    [FEAT] 하단 CollectionView 레이아웃 구성

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/MainBottomTVC.swift
@@ -0,0 +1,70 @@
+//
+//  MainBottomTVC.swift
+//  ABLY-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/15.
+//
+
+import UIKit
+
+class MainBottomTVC: UITableViewCell {
+    static let identifier = "MainBottomTVC"
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var itemCollectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUI()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
+
+extension MainBottomTVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension MainBottomTVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.size.width - 44 - 12
+        return CGSize(width: width, height: 265)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 38
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 23, left: 22, bottom: 38, right: 22)
+    }
+}
+
+extension MainBottomTVC {
+    private func setUI() {
+        setCollectionView()
+        setLabel()
+    }
+    
+    private func setCollectionView() {
+        itemCollectionView.delegate = self
+        itemCollectionView.dataSource = self
+    }
+    
+    private func setLabel() {
+        titleLabel.text = "최성아님을 위한 추천상품"
+        titleLabel.font = .boldSystemFont(ofSize: 12)
+    }
+}
