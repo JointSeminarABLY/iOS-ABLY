307805867ad711d96da2b5e382b9d2f5881b1484
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-15 22:13:49 +0900

    [ADD] 추천상품 셀 추가

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/CVC/RecommendItemCVC.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendItemCVC.swift
+//  ABLY-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/15.
+//
+
+import UIKit
+
+class RecommendItemCVC: UICollectionViewCell {
+    static let identifier = "RecommendItemCVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
