34c6d7ba316eca9873631d2304c2f6b256292267
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-15 23:39:44 +0900

    [FEAT] HotRank 세부사항 설정

--- ABLY-iOS/ABLY-iOS/Source/Cells/TVC/MainHotRankTVC.swift
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/MainHotRankTVC.swift
@@ -10,8 +10,14 @@ import UIKit
 class MainHotRankTVC: UITableViewCell {
     static let identifier = "MainHotRankTVC"
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var medalImageView: UIImageView!
+    @IBOutlet weak var moreButton: UIButton!
+    @IBOutlet weak var hotRankCollectionView: UICollectionView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setUI()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,3 +25,56 @@ class MainHotRankTVC: UITableViewCell {
     }
 }
 
+extension MainHotRankTVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension MainHotRankTVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (UIScreen.main.bounds.size.width - 24) / 4
+        return CGSize(width: width, height: 135)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 26, left: 24, bottom: 26, right: 24)
+    }
+}
+
+// MARK: - UI
+extension MainHotRankTVC {
+    private func setUI() {
+        setCollectionView()
+        setLabel()
+        setImage()
+    }
+    
+    private func setCollectionView() {
+        hotRankCollectionView.delegate = self
+        hotRankCollectionView.dataSource = self
+    }
+    
+    private func setLabel() {
+        titleLabel.text = "HOT 🔥 실시간 원피스 랭킹"
+        titleLabel.font = .boldSystemFont(ofSize: 12)
+    }
+    
+    private func setImage() {
+        // MARK: - asset 가져온 다음에 넣어주기
+        moreButton.setImage(UIImage(named: "more"), for: .normal)
+        medalImageView.image = UIImage(named: "medal")
+    }
+}
