3e2315369792f46df9ace8c126b9e7694035779b
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-16 00:58:52 +0900

    [ADD] Detail Slowand Cell 추가

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailSlowandItemTVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailSlowandItemTVC.swift
+//  ABLY-iOS
+//
+//  Created by SHIN YOON AH on 2021/05/16.
+//
+
+import UIKit
+
+class DetailSlowandItemTVC: UITableViewCell {
+    static let identifier = "DetailSlowandItemTVC"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
