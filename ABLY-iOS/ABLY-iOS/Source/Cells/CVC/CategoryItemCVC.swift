04cdf31376c46902e817a5c93134578bf9f18c4c
김윤서 <yo26on@soongsil.ac.kr>
2021-05-17 17:33:54 +0900

    [FEAT] CategoryItemCVC 연결

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/CVC/CategoryItemCVC.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryItemCVC.swift
+//  ABLY-iOS
+//
+//  Created by 김윤서 on 2021/05/17.
+//
+
+import UIKit
+
+class CategoryItemCVC: UICollectionViewCell {
+
+    static public let idenetifier : String = "CategoryItemCVC"
+    
+    @IBOutlet weak var itemButton: AblyCategoryButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setData(text : String){
+        itemButton.setTitle(text, for: .normal)
+    }
+
+}
