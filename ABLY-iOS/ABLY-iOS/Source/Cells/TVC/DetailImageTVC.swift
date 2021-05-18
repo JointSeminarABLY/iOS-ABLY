277df46c71e2e846ead45e1df246c76a2f14083d
김윤서 <yo26on@soongsil.ac.kr>
2021-05-18 19:05:23 +0900

    [ADD] DetailImageTVC추가

---
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailImageTVC.swift
@@ -0,0 +1,23 @@
+//
+//  DetailImageTVC.swift
+//  ABLY-iOS
+//
+//  Created by 김윤서 on 2021/05/18.
+//
+
+import UIKit
+
+class DetailImageTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
