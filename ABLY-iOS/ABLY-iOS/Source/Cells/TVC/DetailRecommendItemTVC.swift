a80af9d25ffe3e4cf9f6c2819f3575387d349dc1
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-05-21 19:28:31 +0900

    [FEAT] detail bottom dummy data

--- ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailRecommendItemTVC.swift
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailRecommendItemTVC.swift
@@ -15,8 +15,11 @@ class DetailRecommendItemTVC: UITableViewCell {
     @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var itemCollectionView: UICollectionView!
     
+    var items: [Item] = []
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setData()
         setUI()
     }
 
@@ -34,6 +37,11 @@ extension DetailRecommendItemTVC: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailItemCVC.identifier, for: indexPath) as? DetailItemCVC else {
             return UICollectionViewCell()
         }
+        let row = items[indexPath.item]
+        cell.cellConfigure(image: row.image,
+                           discount: row.discount ?? 0,
+                           price: row.price,
+                           item: row.item)
         return cell
     }
 }
@@ -91,5 +99,20 @@ extension DetailRecommendItemTVC {
     
     private func setView() {
         bottomView.backgroundColor = .ablyLightGray
+    }
+    
+    private func setData() {
+        items.append(contentsOf: [
+            Item(image: "rectangle3210", discount: 10, price: "36,000", store: "모코블링", item: "블루밍 티셔츠-denim.ts", review: 10, satisfaction: 100),
+            Item(image: "rectangle3211", discount: 15, price: "16,000", store: "예냥냥", item: "빌즈 썸머 밴딩코튼팬츠", review: 49, satisfaction: 91),
+            Item(image: "rectangle3212", discount: 20, price: "13,900", store: "SONA", item: "턴온트레이닝세트(TR)", review: 52, satisfaction: 96),
+            Item(image: "rectangle3213", discount: 24, price: "14,500", store: "소현", item: "소피 배색카라 리본 체크 반팔원피스(OP)", review: 37, satisfaction: 98),
+            Item(image: "rectangle3214", discount: 30, price: "8,900", store: "dear.my", item: "슬림 베이직 스퀘어넥 반팔티(T)", review: 12, satisfaction: 78),
+            Item(image: "rectangle3215", discount: 10, price: "36,000", store: "모코블링", item: "블루밍 티셔츠-denim.ts", review: 10, satisfaction: 100),
+            Item(image: "rectangle3216", discount: 15, price: "16,000", store: "예냥냥", item: "빌즈 썸머 밴딩코튼팬츠", review: 49, satisfaction: 91),
+            Item(image: "rectangle3218", discount: 10, price: "13,900", store: "SONA", item: "턴온트레이닝세트(TR)", review: 52, satisfaction: 96),
+            Item(image: "rectangle3213", discount: 24, price: "14,500", store: "소현", item: "소피 배색카라 리본 체크 반팔원피스(OP)", review: 37, satisfaction: 98),
+            Item(image: "rectangle3214", discount: 50, price: "8,900", store: "dear.my", item: "슬림 베이직 스퀘어넥 반팔티(T)", review: 12, satisfaction: 78)
+        ])
     }
 }
--- ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailSlowandItemTVC.swift
+++ ABLY-iOS/ABLY-iOS/Source/Cells/TVC/DetailSlowandItemTVC.swift
@@ -18,8 +18,11 @@ class DetailSlowandItemTVC: UITableViewCell {
     @IBOutlet weak var moreButton: UIButton!
     @IBOutlet weak var itemCollectionView: UICollectionView!
     
+    var items: [Item] = []
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        setData()
         setUI()
     }
 
@@ -37,6 +40,11 @@ extension DetailSlowandItemTVC: UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailItemCVC.identifier, for: indexPath) as? DetailItemCVC else {
             return UICollectionViewCell()
         }
+        let row = items[indexPath.item]
+        cell.cellConfigure(image: row.image,
+                           discount: row.discount ?? 0,
+                           price: row.price,
+                           item: row.item)
         return cell
     }
 }
@@ -91,9 +99,10 @@ extension DetailSlowandItemTVC {
     }
     
     private func setImage() {
-        // MARK: - asset 가져온 다음에 넣어주기
-        storeImageView.image = UIImage(named: "storeprofile")
-        moreItemButton.setImage(UIImage(named: "button"), for: .normal)
+        storeImageView.image = UIImage(named: "ellipse11")
+        moreItemButton.setTitle("마켓 신상보기", for: .normal)
+        moreItemButton.titleLabel?.font = .systemFont(ofSize: 12, weight: .medium)
+        moreItemButton.setImage(UIImage(named: "icMoresmall"), for: .normal)
     }
     
     private func setButton() {
@@ -104,6 +113,21 @@ extension DetailSlowandItemTVC {
         moreButton.layer.borderWidth = 1
         moreButton.layer.borderColor = UIColor.ablyMediumGray.cgColor
         moreButton.layer.cornerRadius = 10
+    }
+    
+    private func setData() {
+        items.append(contentsOf: [
+            Item(image: "rectangle3210", discount: 10, price: "36,000", store: "모코블링", item: "블루밍 티셔츠-denim.ts", review: 10, satisfaction: 100),
+            Item(image: "rectangle3211", discount: 15, price: "16,000", store: "예냥냥", item: "빌즈 썸머 밴딩코튼팬츠", review: 49, satisfaction: 91),
+            Item(image: "rectangle3212", discount: 20, price: "13,900", store: "SONA", item: "턴온트레이닝세트(TR)", review: 52, satisfaction: 96),
+            Item(image: "rectangle3213", discount: 24, price: "14,500", store: "소현", item: "소피 배색카라 리본 체크 반팔원피스(OP)", review: 37, satisfaction: 98),
+            Item(image: "rectangle3214", discount: 30, price: "8,900", store: "dear.my", item: "슬림 베이직 스퀘어넥 반팔티(T)", review: 12, satisfaction: 78),
+            Item(image: "rectangle3215", discount: 10, price: "36,000", store: "모코블링", item: "블루밍 티셔츠-denim.ts", review: 10, satisfaction: 100),
+            Item(image: "rectangle3216", discount: 15, price: "16,000", store: "예냥냥", item: "빌즈 썸머 밴딩코튼팬츠", review: 49, satisfaction: 91),
+            Item(image: "rectangle3218", discount: 10, price: "13,900", store: "SONA", item: "턴온트레이닝세트(TR)", review: 52, satisfaction: 96),
+            Item(image: "rectangle3213", discount: 24, price: "14,500", store: "소현", item: "소피 배색카라 리본 체크 반팔원피스(OP)", review: 37, satisfaction: 98),
+            Item(image: "rectangle3214", discount: 50, price: "8,900", store: "dear.my", item: "슬림 베이직 스퀘어넥 반팔티(T)", review: 12, satisfaction: 78)
+        ])
     }
 }
 
